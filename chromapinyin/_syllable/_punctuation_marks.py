602e61a843731eba33a1659166ce283bcd6123fa
Travis G_____ <162270915+travisgk@users.noreply.github.com>
2024-11-04 11:47:15 -0600

    Delete chromapinyin directory

--- chromapinyin/_syllable/_punctuation_marks.py
+++
@@ -1,8 +0,0 @@
-# chromapinyin._syllable._punctuation_marks.py
-# ---
-# this file defines character constants that are used across the program.
-#
-
-APOSTROPHES = "'’"
-PUNCTUATION = APOSTROPHES + "。，！？；：（）、．—～.,!-?;:"
-CLAUSE_BREAKS = "。，！？；：、．.,!-?;:"
